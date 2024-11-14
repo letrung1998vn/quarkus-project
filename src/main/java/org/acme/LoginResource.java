3f4b7709e0ff06579ef9014f99b917dd1def1c16
le.trung01 <40650762+letrung1998vn@users.noreply.github.com>
2024-11-14 16:54:07 +0700

    add bcrypt

--- src/main/java/org/acme/LoginResource.java
+++ src/main/java/org/acme/LoginResource.java
@@ -1,16 +1,20 @@
 package org.acme;
 
+import io.quarkus.elytron.security.common.BcryptUtil;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import lombok.NonNull;
+import org.acme.bean.LoginBean;
 import org.acme.dao.AccountDAO;
 import org.acme.dto.AccountDTO;
-import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestResponse;
 
-import java.util.Base64;
+import java.util.List;
 
 @Path("/auth")
 public class LoginResource {
@@ -21,41 +25,24 @@ public class LoginResource {
         this.dao = dao;
     }
 
-    private static boolean checkString(String str) {
-        char ch;
-        boolean capitalFlag = false;
-        boolean lowerCaseFlag = false;
-        boolean numberFlag = false;
-        for (int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            if (Character.isDigit(ch)) {
-                numberFlag = true;
-            } else if (Character.isUpperCase(ch)) {
-                capitalFlag = true;
-            } else if (Character.isLowerCase(ch)) {
-                lowerCaseFlag = true;
-            } else if (Character.isSpaceChar(ch)) {
-                return false;
-            }
-            if (numberFlag && capitalFlag && lowerCaseFlag)
-                return true;
-        }
-        return false;
+    public static void main(String[] args) {
+        System.out.println(BcryptUtil.bcryptHash("Admin12345"));
     }
 
     @Path("/login")
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public RestResponse<String> login(@RestForm String userName, @RestForm String password) {
-        String encodePass = new String(Base64.getEncoder().encode(password.getBytes()));
-        if (password.length() <= 8 || password.length() >= 16 || !checkString(password)) {
-            return RestResponse.ResponseBuilder.create(RestResponse.Status.BAD_REQUEST, "Invalid password format").build();
-        }
-
-        AccountDTO dto = dao.findByUsernameAndPassword(userName, encodePass);
-        if (dto != null) {
-            return RestResponse.ResponseBuilder.ok("Welcome " + userName, MediaType.TEXT_PLAIN_TYPE).build();
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public RestResponse<String> login(@NotNull @Valid LoginBean login) {
+        List<AccountDTO> dto = dao.findByUsernameAndPassword(login.getUserName());
+        if (dto.isEmpty()) {
+            return RestResponse.ResponseBuilder.create(RestResponse.Status.BAD_REQUEST, "Incorrect username or password").build();
         } else {
+            for (AccountDTO account : dto) {
+                if(BcryptUtil.matches(login.getPassword(),account.getPassword())){
+                    return RestResponse.ResponseBuilder.ok("Welcome " + account.getUserName(), MediaType.TEXT_PLAIN_TYPE).build();
+                }
+            }
             return RestResponse.ResponseBuilder.create(RestResponse.Status.BAD_REQUEST, "Incorrect username or password").build();
         }
     }
--- src/main/java/org/acme/dao/AccountDAO.java
+++ src/main/java/org/acme/dao/AccountDAO.java
@@ -15,12 +15,7 @@ public class AccountDAO {
     EntityManager em;
 
     @Transactional
-    public AccountDTO findByUsernameAndPassword(String username, String password) {
-        List<AccountDTO> result = em.createQuery("select userName, password from account where userName= :value1 and password=:value2", AccountDTO.class).setParameter("value1", username).setParameter("value2", password).getResultList();
-        if (!result.isEmpty()) {
-            return result.getFirst();
-        } else {
-            return null;
-        }
+    public List<AccountDTO> findByUsernameAndPassword(String username) {
+        return em.createQuery("select userName, password from account where userName= :value1 ", AccountDTO.class).setParameter("value1",username).getResultList();
     }
 }
